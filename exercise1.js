0e7aa590049f1d752226933b21f55b34ac141481
Jeronimo Tzib <2023159736@ub.edu.bz>
2026-02-04 11:25:26 -0600

    part 1 correction

--- exercise1.js
+++ exercise1.js
@@ -5,3 +5,13 @@ function calculateArea (length, width){
 console.log(calculateArea(5, 10)); // Output: 50
 console.log(calculateArea(7, 3));  // Output: 21
 console.log(calculateArea(0, 10)); // Output: 0
+
+const calculatePerimiter = function (length, width){
+
+return 2 * (length + width)
+
+}
+
+console.log(calculatePerimiter(5, 10)); // Output: 30
+console.log(calculatePerimiter(7, 3));  // Output: 20
+console.log(calculatePerimiter(0, 10)); // Output: 20
