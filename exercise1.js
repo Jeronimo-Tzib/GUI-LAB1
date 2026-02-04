5cfa17e0c333888359bd19dd6c2d849e421465eb
Jeronimo Tzib <2023159736@ub.edu.bz>
2026-02-04 11:20:43 -0600

    part 1 commit

---
+++ exercise1.js
@@ -0,0 +1,7 @@
+function calculateArea (length, width){
+    return length * width;
+}
+
+console.log(calculateArea(5, 10)); // Output: 50
+console.log(calculateArea(7, 3));  // Output: 21
+console.log(calculateArea(0, 10)); // Output: 0
---
+++ exercise2.js
@@ -0,0 +1,9 @@
+const calculatePerimiter = function (length, width){
+
+return 2 * (length + width)
+
+}
+
+console.log(calculatePerimiter(5, 10)); // Output: 30
+console.log(calculatePerimiter(7, 3));  // Output: 20
+console.log(calculatePerimiter(0, 10)); // Output: 20
